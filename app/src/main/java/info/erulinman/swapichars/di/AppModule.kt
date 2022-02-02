4e72ec40d094e32b928e9d44245f506a1a2723db
erulinman <erwinstein.contact@gmail.com>
2022-02-02 15:20:30 +0300

    Set AppModule providers as singletons

--- app/src/main/java/info/erulinman/swapichars/di/AppModule.kt
+++ app/src/main/java/info/erulinman/swapichars/di/AppModule.kt
@@ -4,13 +4,16 @@ import android.content.Context
 import android.content.res.Resources
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
-@Module
+@Module(includes = [NetworkModule::class, DatabaseModule::class])
 class AppModule(private val context: Context) {
 
+    @Singleton
     @Provides
     fun provideContext(): Context = context
 
+    @Singleton
     @Provides
     fun provideResources(context: Context): Resources = context.resources
 }
